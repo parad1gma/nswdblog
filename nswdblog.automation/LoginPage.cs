2ecdf6683ab2f642b00936538897509331c4bce9
parad1gma <parad1gma@paradigma.rs>
2016-12-15 14:14:14 +0100

    Creating The Driver

---
+++ nswdblog.automation/LoginPage.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace nswdblog.automation
+{
+    public class LoginPage
+    {
+        public static void GoTo()
+        {
+            Driver.Instance.Navigate().GoToUrl("https://wordpress.com/me");
+        }
+
+        public static LoginCommand LoginAs(string username)
+        {
+            return new LoginCommand(username);
+        }
+    }
+
+    public class LoginCommand
+    {
+        private readonly string username;
+        private string password;
+
+        public LoginCommand(string username)
+        {
+            this.username = username;
+        }
+
+        public LoginCommand WithPassword(string password)
+        {
+            this.password = password;
+            return this;
+        }
+
+        public void Login()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ nswdblog.tests/LoginTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using nswdblog.automation;
+
+namespace nswdblog.tests
+{
+    [TestClass]
+    public class LoginTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            Driver.Initialize();
+        }
+
+        [TestMethod]
+        public void AdminUserCanLogin()
+        {
+            LoginPage.GoTo();
+            LoginPage.LoginAs("mile.kordic@nswebdevelopment.com").WithPassword("Parad1gma").Login();
+
+            Assert.IsTrue(MyProfile.IsAt, "Failed to login.");
+        }
+    }
+}
