c1dc476936da0491cfe4cc347d2dc5f27c9e205d
parad1gma <parad1gma@paradigma.rs>
2016-12-15 13:15:34 +0100

    Add project files.

---
+++ nswdblog.automation/Class1.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium.Firefox;
+
+namespace nswdblog.automation
+{
+    public class Class1
+    {
+        public void Go()
+        {
+            var driver = new FirefoxDriver();
+            driver.Navigate
+        }
+    }
+}
